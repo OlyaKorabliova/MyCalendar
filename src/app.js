a6f610b89bcd3ea5d2a198b1c5bbbe1cbbfbee74
olya <korablyova@ucu.edu.ua>
2019-09-29 16:58:57 +0300

    refactored calendar components

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout"
 
-ReactDOM.render(<Layout date={new Date()}/>, document.querySelector(".Layout"));
+ReactDOM.render(<Layout />, document.querySelector(".Layout"));
